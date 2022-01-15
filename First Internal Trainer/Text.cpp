813ebd91a5ff6af5c1d8c5fe54d61ca5e81adbb3
tubaplayerdis <70219635+tubaplayerdis@users.noreply.github.com>
2022-01-15 14:39:50 -0600

    getting readt for textin opengl

---
+++ First Internal Trainer/Text.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
